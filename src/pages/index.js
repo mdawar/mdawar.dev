2b83e020131bd4858ac93c64fec5dcc97b846f05
Pierre Mdawar <pierre@mdawar.dev>
2020-05-04 10:52:25 +0300

    Added basic blog posts query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,49 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql } from 'gatsby';
 
-export default () => {
+export default ({ data }) => {
   const {
     site: {
-      siteMetadata: { title, description },
-    },
-  } = useStaticQuery(
-    graphql`
-      query SiteMetadata {
-        site {
-          siteMetadata {
-            title
-            description
-          }
-        }
-      }
-    `
-  );
+      siteMetadata: { title, description }
+    }
+  } = data;
+
+  const posts = data.allMdx.nodes;
 
   return (
     <>
       <h1>{title}</h1>
       <p>{description}</p>
+      {posts.map(({ id, excerpt, frontmatter }) => (
+        <div key={id}>
+          <h3>{frontmatter.title}</h3>
+          <p>Posted on: {frontmatter.date}</p>
+          <p>{excerpt}</p>
+        </div>
+      ))}
     </>
   );
 };
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+
+    allMdx {
+      nodes {
+        frontmatter {
+          title
+          description
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        excerpt
+      }
+    }
+  }
+`;
