70acce86ac1f8112f7807319628e2de9c09af6e4
Pierre Mdawar <pierre@mdawar.dev>
2020-05-05 20:19:57 +0300

    Added all tags page

---
+++ src/pages/tags.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+export default ({ data }) => {
+  const tags = data.allMdx.group;
+
+  return (
+    <div>
+      <h1>Tags</h1>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag.name}>
+            <Link to={`/tags/${tag.name.toLowerCase()}/`}>
+              {tag.name} ({tag.totalCount})
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query AllTags {
+    allMdx(limit: 2000) {
+      group(field: frontmatter___tags) {
+        name: fieldValue
+        totalCount
+      }
+    }
+  }
+`;
