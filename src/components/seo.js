4234a9de22f23338203994739e4f3069e7e9f1fb
Pierre Mdawar <pierre@mdawar.dev>
2020-05-28 13:06:46 +0300

    [SEO] added Open Graph meta tags

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,11 +23,20 @@ export default function SEO({ title, author, description, keywords, path }) {
     <Helmet titleTemplate={`%s - ${site.title}`} defaultTitle={site.title}>
       <html lang="en" />
       <meta charSet="utf-8" />
+
       {title && <title>{title}</title>}
       {author && <meta name="author" content={author} />}
       {description && <meta name="description" content={description} />}
       {keywords && <meta name="keywords" content={keywords.join(', ')} />}
       {path && <link rel="canonical" href={`${site.url}${path}`} />}
+
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:site_name" content={site.title} />
+      {title && <meta property="og:title" content={title} />}
+      {description && <meta property="og:description" content={description} />}
+      {path && <meta property="og:url" content={`${site.url}${path}`} />}
+
       {(title || description) && <meta name="twitter:card" content="summary" />}
       {title && <meta name="twitter:title" content={title} />}
       {description && <meta name="twitter:description" content={description} />}
