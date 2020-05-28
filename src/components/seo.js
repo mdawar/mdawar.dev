f0e5265bbb9e153bc7ae0d1975f997795dad5b92
Pierre Mdawar <pierre@mdawar.dev>
2020-05-28 11:51:44 +0300

    [SEO] import the graphql tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { useStaticQuery } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export default function SEO({ title, author, description, keywords, path }) {
   const {
