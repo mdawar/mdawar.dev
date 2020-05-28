4d52b753a44747ffde89f4c2ee8e45211c888b06
Pierre Mdawar <pierre@mdawar.dev>
2020-05-28 12:02:31 +0300

    Added gatsby-remark-images and sharp plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -16,7 +17,23 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
-        extensions: ['.mdx', '.md']
+        extensions: ['.mdx', '.md'],
+        gatsbyRemarkPlugins: [
+          {
+            // Process images in the MDX files using sharp (only JPEG and PNG)
+            // GIFs and SVGs are not supported
+            resolve: 'gatsby-remark-images',
+            options: {
+              // Max width in pixels of the content container
+              // Used by the plugin as the base for generating the images
+              maxWidth: 800,
+              quality: 50,
+              loading: 'lazy',
+              linkImagesToOriginal: true,
+              showCaptions: false
+            },
+          },
+        ]
       }
     }
   ]
