050ebdb006ecbeb7f7bd87894b024685cb00e42c
Pierre Mdawar <pierre@mdawar.dev>
2020-06-12 14:53:34 +0300

    [Projects] open the links in a new tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from './Link';
 import { Section, SectionHeader } from '../styles/elements/content';
 
 export default function Projects() {
@@ -8,11 +9,11 @@ export default function Projects() {
         <h2>Open Source Projects</h2>
       </SectionHeader>
       <h3>
-        <a href="https://github.com/mdawar/terraformjs">TerraformJS</a>
+        <Link href="https://github.com/mdawar/terraformjs">TerraformJS</Link>
       </h3>
       <p>Infrastructure as code using JavaScript and Terraform.</p>
       <h3>
-        <a href="https://github.com/mdawar/rq-exporter">RQ Exporter</a>
+        <Link href="https://github.com/mdawar/rq-exporter">RQ Exporter</Link>
       </h3>
       <p>Prometheus metrics exporter for Python RQ (Redis Queue).</p>
     </Section>
